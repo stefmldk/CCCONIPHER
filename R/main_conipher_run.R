873bcdf1323fcb2f3dd180bce33cf1cc51140986
stefmldk <98875215+stefmldk@users.noreply.github.com>
2024-10-03 13:00:22 +0200

    Added support for custom colors

--- R/main_conipher_run.R
+++ R/main_conipher_run.R
@@ -8,9 +8,21 @@
 
 conipher_run <- function(case_id, prefix, out_dir, input_tsv_loc, ...) {
     out_dir_tmp <- paste0(out_dir, "/Clustering/")
-    conipher_clustering(case_id = case_id, 
-                        out_dir = out_dir_tmp, 
-                        input_tsv_loc = input_tsv_loc, 
+
+    # In case a run_conipher function that does not set custom_colors is used
+    if(missing(custom_colors)){
+        custom_colors <- ()
+    }
+    if (length(custom_colors)) {
+        print('Running CONIPHER with custom colors...')
+    }
+
+    # Make custom_colors globally available since it is referenced in som other modules that will not get it via calls to conipher_clustering or conipher_treebuilding
+    assign("custom_colors", custom_colors, envir = .GlobalEnv)
+
+    conipher_clustering(case_id = case_id,
+                        out_dir = out_dir_tmp,
+                        input_tsv_loc = input_tsv_loc,
                         ...)
     tree_input_tsv_loc <- paste0(out_dir_tmp, case_id, ".SCoutput.CLEAN.tsv")
     out_dir_tmp <- paste0(out_dir, "/Trees/")

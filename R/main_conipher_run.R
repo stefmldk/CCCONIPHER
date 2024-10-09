6b5462765f638e3b8a5119c37e0e97c456ea64b1
stefmldk <98875215+stefmldk@users.noreply.github.com>
2024-10-09 13:35:46 +0200

    Update main_conipher_run.R

--- R/main_conipher_run.R
+++ R/main_conipher_run.R
@@ -6,15 +6,16 @@
 #' @returns NULL
 #' @export conipher_run
 
-conipher_run <- function(case_id, prefix, out_dir, input_tsv_loc, custom_colors, ...) {
+conipher_run <- function(case_id, prefix, out_dir, input_tsv_loc, custom_colors, max_alt_trees, ...) {
     out_dir_tmp <- paste0(out_dir, "/Clustering/")
 
     if (length(custom_colors)) {
         cat('\nRunning CONIPHER with custom colors...\n')
     }
 
-    # Make custom_colors globally available since it is referenced in som other modules that will not get it via calls to conipher_clustering or conipher_treebuilding
+    # Make custom_colors and max_alt_trees globally available since they are referenced in some other modules that will not get them via function params
     assign("custom_colors", custom_colors, envir = .GlobalEnv)
+    assign("max_alt_trees", max_alt_trees, envir = .GlobalEnv)
 
     conipher_clustering(case_id = case_id,
                         out_dir = out_dir_tmp,

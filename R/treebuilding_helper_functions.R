a39baf3118e816c71d7ec74a00d6c4d0d01c56b8
stefmldk <98875215+stefmldk@users.noreply.github.com>
2025-10-28 13:46:19 +0100

    Update treebuilding_helper_functions.R

--- R/treebuilding_helper_functions.R
+++ R/treebuilding_helper_functions.R
@@ -917,14 +917,24 @@ grow.multi.trees <- function(nestedlist, graph_pyclone, pyclone, ccf_buffer = 10
       previous.tree <- old.tree
       old.tree.expanded <- expand.tree(previous.tree, trunk_cluster)
 
+      print()
+      print('### previous.tree')
+      print(previous.tree)
+      print('### old.tree.expanded')
+      print(old.tree.expanded)
+
       new.tree <- old.tree
       nestedclust_test <- nestedclust
       nestedclust_test[keycluster, cluster1] <- 0
       nesting.removed <- paste(cluster1, keycluster, sep = ":")
       nesting.to.keep <- cbind(colnames(nestedclust_test)[nestedclust_test[keycluster,] == 1], keycluster)
-      print('\n')
+      print()
+      print('### nestedclust')
+      print(nestedclust)
       print('### nestedclust_test')
-      print(nestedclust_test)
+      print(nestedclust)
+      print('### nesting.removed')
+      print(nesting.removed)
       print('### keycluster')
       print(keycluster)
       print('### nesting.to.keep')
